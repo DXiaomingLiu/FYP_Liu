e2b13943824ae20354f2f7aab8020aedbb84c293
DXiaomingLiu <xiao.liu16@imperial.ac.uk>
2019-03-12 18:21:28 +0000

    Fourth commit: added vcf to numpy array of snp positions functionality. Fixed output genotype array dimension in vcf2numpy. Started building a multi-input two-branch convnet and densely connected neural network - outlined by Schrider (2018).

--- vcf2numpy.py
+++ vcf2numpy.py
@@ -1,28 +1,55 @@
-###Xiaoming Liu 07/03/2019
+###Xiaoming Liu 11/03/2019
 #This scirpt takes vcfs of individual windows and
 #load them into numpy arrays
 ###
 #Building dependencies
 import allel
 import glob
+import numpy as np
 #os.chdir("/FYP_Liu")
-vcfs = []
+snps = []
+pos = []
+headers = []
 for file in glob.glob("./*.vcf"):
-    vcfs.append(allel.read_vcf(file))
+    item = allel.GenotypeArray(allel.read_vcf(file)['calldata/GT'])
+    dim = item.shape
+    print(dim)
+    pos.append(allel.read_vcf(file)['variants/POS'])
+    snps.append(item.reshape(dim[0],dim[1]*dim[2],1))
+    headers.append(allel.read_vcf_headers(file))
+
+    print(len(headers[0][4]))
 
 #print(vcfs[0]['samples'])
 #print(type(vcfs[0]))
+#print(headers[0][4])
+#print(type(headers[0]))
 
-gf = allel.GenotypeArray(vcfs[0]['calldata/GT'])
-print(gf)
+#gfs = np.zeros(len(vcfs),len(vcfs[0]))
+#gf = allel.GenotypeArray(vcfs[0]['calldata/GT'])
+#print(gf)
+#for i in range(len(vcfs)):
+#    vcfs[i] = allel.GenotypeArray(vcfs[0]['calldata/GT']).to_n_alt()
+
 
 def plot_ld(gn, title):
     m = allel.rogers_huff_r(gn) ** 2
     ax = allel.plot_pairwise_ld(m)
     ax.set_title(title)
 
-gn = gf.to_n_alt()
+#gn = gf.to_n_alt()
 
-print(gn)
-p = plot_ld(gn[:1000], 'Figure 1. Pairwise LD.')
-print(p)
+#print(gn)
+#print(gn.shape)
+#print(type(gn))
+#p = plot_ld(gn[:1000], 'Figure 1. Pairwise LD.')
+#print(p)
+snps = np.asarray(snps)
+pos = np.asarray(pos)
+print(snps.shape)
+print(snps)
+print(len(np.where(snps > 0)[-1])/(3445*5008))
+print(pos)
+print(pos.shape)
+np.save('test_snp.npy',snps)
+np.save('test_pos.npy',pos)
